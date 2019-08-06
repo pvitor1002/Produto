e70118d383c0132799fa359da018e6789168d4cf
Paulo Vitor Maia Carneiro <paulo.vitor.carneiro@ibm.com>
2019-08-06 15:12:02 -0300

    commit inicial

---
+++ src/main/java/com/Produto/Aplication/ProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.Produto.Aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoApplication.class, args);
+	}
+
+}
